24ae4f94714106f6240d050da4c9a82304dfda72
Sebastien Wieckowski <sbwiecko@free.fr>
2023-09-01 09:14:41 +0200

    completed ch4/part I

--- Part_1_Basic_Toolkit/Ch.04_Webdev101/script.js
+++ Part_1_Basic_Toolkit/Ch.04_Webdev101/script.js
@@ -1,3 +1,38 @@
+var chartCircles = function (data) {
+  var chart = d3.select("#chart");
+  // Set the chart height and width from data
+  chart.attr("height", data.height).attr("width", data.width);
+  // Create some circles using the data
+  chart
+    .selectAll("circle")
+    .data(data.circles)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return d.x;
+    })
+    .attr("cy", function (d) {
+      return d.y;
+    })
+    .attr("r", function (d) {
+      return d.r;
+    });
+};
+
+var data = {
+  width: 300,
+  height: 150,
+  circles: [
+    { x: 50, y: 30, r: 20 },
+    { x: 70, y: 80, r: 10 },
+    { x: 160, y: 60, r: 10 },
+    { x: 200, y: 100, r: 5 },
+  ],
+};
+
+chartCircles(data);
+
+
 function buildChart() {
   var padding = 20;
   var height = 150,
@@ -56,41 +91,7 @@ function buildChart() {
   return;
 }
 
-var chartCircles = function (data) {
-  var chart = d3.select("#chart");
-  // Set the chart height and width from data
-  chart.attr("height", data.height).attr("width", data.width);
-  // Create some circles using the data
-  chart
-    .selectAll("circle")
-    .data(data.circles)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) {
-      return d.x;
-    })
-    .attr("cy", function (d) {
-      return d.y;
-    })
-    .attr("r", function (d) {
-      return d.r;
-    });
-};
-
-var data = {
-  width: 300,
-  height: 150,
-  circles: [
-    { x: 50, y: 30, r: 20 },
-    { x: 70, y: 80, r: 10 },
-    { x: 160, y: 60, r: 10 },
-    { x: 200, y: 100, r: 5 },
-  ],
-};
-
-chartCircles(data);
-
-// buildChart();
+buildChart();
 
 // main javascript here:
 // d3.select('#chart svg')
